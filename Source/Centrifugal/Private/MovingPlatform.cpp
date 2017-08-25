e2c5e34b0d6aeca6d3068cce45a86d0afb19d79b
Steven <onlinecop@gmail.com>
2017-08-24 23:00:56 -0600

    First pass: using the same parameters that are used in the MovingPlatform BP.

--- Source/Centrifugal/Private/MovingPlatform.cpp
+++ Source/Centrifugal/Private/MovingPlatform.cpp
@@ -1,27 +1,68 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "MovingPlatform.h"
+#include "..\Public\MovingPlatform.h"
 
 
 // Sets default values
 AMovingPlatform::AMovingPlatform()
+	: bLoop(true)
+	, bMovePlatform(true)
+	, bWaitAtEndpoints(true)
+	, bStopOnHit(true)
+	, bUseTrackRoll(false)
+	, buseTrackPitch(false)
+	, bUseTrackYaw(false)
+	, PlatformEndpointWaitTime(1.0f)
+	, PlatformStartTime(0.0f)
+	, TrackDuration(10.0f)
+	, _PlatformTime(0.0f)
+	, _DeltaTime(0.0f)
+	, _CachedPlatformTime(0.0f)
+	, PlatformMovement(EPlatformMovement::PingPong)
+	, PlatformStartDirection(EPlatformDirection::Forward)
+	, _PlatformDirection(EPlatformDirection::Forward)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 }
 
+void AMovingPlatform::UpdatePlatformTime(USplineComponent* TrackSpline)
+{
+	_CachedPlatformTime = _PlatformTime;
+
+	float adjustedPlatformTime = 0.0f;
+	if (_PlatformDirection == EPlatformDirection::Forward)
+	{
+		adjustedPlatformTime = _DeltaTime + _PlatformTime;
+	}
+	else
+	{
+		adjustedPlatformTime = -_DeltaTime + _PlatformTime;
+	}
+	float splineDuration = 0.0f;
+	if (TrackSpline != nullptr)
+	{
+		splineDuration = TrackSpline->Duration;
+	}
+
+	_PlatformTime = FMath::Clamp<float>(adjustedPlatformTime, 0.0f, splineDuration);
+}
+
 // Called when the game starts or when spawned
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+	_DeltaTime = DeltaTime;
+	if (bMovePlatform)
+	{
 
+	}
 }
-
--- Source/Centrifugal/Public/MovingPlatform.h
+++ Source/Centrifugal/Public/MovingPlatform.h
@@ -6,23 +6,113 @@
 #include "GameFramework/Actor.h"
 #include "MovingPlatform.generated.h"
 
+
+UENUM(BlueprintType)
+enum class EPlatformMovement : uint8
+{
+	// Reverse direction once the platform reaches the end of the spline track.
+	PingPong,
+	// Snap platform to beginning of spline track once it reaches the end of the spline track.
+	LoopFromBeginning,
+};
+
+
+UENUM(BlueprintType)
+enum class EPlatformDirection : uint8
+{
+	// Platform is moving from start-to-end.
+	Forward,
+	// Platform is moving from end-to-start.
+	Backward
+};
+
+
 UCLASS()
 class CENTRIFUGAL_API AMovingPlatform : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AMovingPlatform();
+
+	void SetPlatformTime(float val) { _PlatformTime = val; }
+	void SetDeltaTime(float val) { _DeltaTime = val; }
+	void SetCachedPlatformTime(float val) { _CachedPlatformTime = val; }
+	void SetPlatformDirection(EPlatformDirection val) { _PlatformDirection = val; }
+
+	UFUNCTION(BlueprintCallable, Category = "Platform")
+	void UpdatePlatformTime(USplineComponent* TrackSpline);
+
+	/** Whether the platform loops when it reaches an end point. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	bool bLoop;
+
+	/** Whether the platform moves. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	bool bMovePlatform;
+
+	/** Whether the platform waits when it reaches the beginning or end of its track. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	bool bWaitAtEndpoints;
+
+	/** Stop the platform when it hits a solid object. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	bool bStopOnHit;
+
+	/** Whether we use the track's roll rotation.
+	 *  *** Warning! Enabling Roll can have unexpected results ***
+	 */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	bool bUseTrackRoll;
+
+	/** Whether we use the track's pitch rotation. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	bool buseTrackPitch;
+
+	/** Whether we use the track's yaw rotation. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	bool bUseTrackYaw;
+
+	/** How long the platform will wait at the beginning and end of its track before moving again. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	float PlatformEndpointWaitTime;
+
+	/** Where the platform starts on the track. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	float PlatformStartTime;
+
+	/** How long it takes to get from one end of the track to the other. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	float TrackDuration;
+
+	/** 1 = forward, -1 = backward. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	EPlatformDirection PlatformStartDirection;
+
+	/** How to handle reaching the beginning or end of the Track spline. */
+	UPROPERTY(BlueprintReadWrite, Category = "Platform")
+	EPlatformMovement PlatformMovement;
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+	/** Time/distance traveled along the path. */
+	UPROPERTY(BlueprintReadOnly, Category = "Platform")
+	float _PlatformTime;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Platform")
+	float _DeltaTime;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Platform")
+	float _CachedPlatformTime;
+
+	/** Is the platform moving forward or backward? */
+	UPROPERTY(BlueprintReadOnly, Category = "Platform")
+	EPlatformDirection _PlatformDirection;
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	
-	
 };
